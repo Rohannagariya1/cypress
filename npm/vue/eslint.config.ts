ce4bbc888c91ea6c52140ac948c8e1cc543f188c
Cacie Prins <cacie@cypress.io>
2025-01-15 16:53:36 -0500

    fix vue eslint config

--- npm/vue/eslint.config.ts
+++ npm/vue/eslint.config.ts
@@ -5,6 +5,15 @@ import globals from 'globals'
 export default [
   ...baseConfig,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'cypress.config.ts', 'vite.config.ts', 'webpack.config.js', 'rollup.config.mjs'],
+        },
+      },
+    },
+  },
+  {
     files: ['**/*.vue'],
     languageOptions: {
       parserOptions: {
--- npm/vue/test-tsd/index.d.ts
+++ npm/vue/test-tsd/index.d.ts
@@ -1,3 +1,5 @@
 export function expectType<T> (value: T): void
+
 export function expectError<T> (value: T): void
+
 export function expectAssignable<T, T2 extends T = T> (value: T2): void
