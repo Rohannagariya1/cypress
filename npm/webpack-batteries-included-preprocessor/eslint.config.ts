1d1d61c6c1ba29ad84977b22f559b205bca0b3e1
Cacie Prins <cacie@cypress.io>
2025-01-15 16:49:39 -0500

    fix webpack-batteries eslint config

--- npm/webpack-batteries-included-preprocessor/eslint.config.ts
+++ npm/webpack-batteries-included-preprocessor/eslint.config.ts
@@ -3,8 +3,14 @@ import globals from 'globals'
 
 export default [
   ...baseConfig,
+  { ignores: ['test/fixtures/**/*', 'index.js'] },
   {
   languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ['eslint.config.ts'],
+      },
+    },
     globals: {
       ...globals.node,
     },
