3e35bce625eca2318f6df48c27f73738211face3
Cacie Prins <cacie@cypress.io>
2025-01-15 16:26:45 -0500

    fix mount-utils eslint config

--- npm/mount-utils/eslint.config.ts
+++ npm/mount-utils/eslint.config.ts
@@ -3,4 +3,13 @@ import globals from 'globals'
 
 export default [
   ...baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'create-rollup-entry.mjs'],
+        },
+      },
+    },
+  },
 ]
