7b44b5669f4a66e8f3475b9642d539d615814037
Cacie Prins <cacie@cypress.io>
2025-01-15 16:29:47 -0500

    fix webpack-preprocessor eslint.config.ts

--- npm/webpack-preprocessor/eslint.config.ts
+++ npm/webpack-preprocessor/eslint.config.ts
@@ -4,10 +4,15 @@ import globals from 'globals'
 export default [
   ...baseConfig,
   {
-    ignores: ['__snapshots__/**/*', 'cypress/tests/e2e/compile-error.js', 'test/fixtures/syntax_error_spec.js'],
+    ignores: ['__snapshots__/**/*', 'cypress/tests/e2e/compile-error.js', 'test/fixtures/**/*'],
   },
   {
     languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['cypress.config.js', 'scripts/test-webpack-5.js'],
+        },
+      },
       globals: {
         ...globals.node,
       },
