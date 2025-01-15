515fb78cf9e8cd654eea5c161a2cfa7d114fd6f9
Cacie Prins <cacie@cypress.io>
2025-01-15 16:57:19 -0500

    fix vite-dev-server eslint

--- npm/vite-dev-server/eslint.config.ts
+++ npm/vite-dev-server/eslint.config.ts
@@ -4,7 +4,15 @@ import globals from 'globals'
 export default [
   ...baseConfig,
   {
+    ignores: ['index.d.ts', 'index.js', 'test/.mocharc.js'],
+  },
+  {
     languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'cypress.config.ts', 'client/initCypressTests.js'],
+        },
+      },
       globals: {
         process: 'readonly',
       },
