be425ab80bd9b528b06a95c2b0c2e2846f6e18e7
Cacie Prins <cacie@cypress.io>
2025-01-16 09:38:07 -0500

    fix lint for npm/webpack-dev-server

--- npm/webpack-dev-server/eslint.config.ts
+++ npm/webpack-dev-server/eslint.config.ts
@@ -4,6 +4,15 @@ import globals from 'globals'
 export default [
   ...baseConfig,
   {
-    ignores: ['__snapshots__/**/*', 'test/fixtures/compilation-fails.spec.js'],
+    ignores: ['__snapshots__/**/*', 'test/fixtures/compilation-fails.spec.js', 'test/.mocharc.js'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'cypress.config.ts'],
+        },
+      },
+    },
   },
 ]
