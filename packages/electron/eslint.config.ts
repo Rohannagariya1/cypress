09582b4072a32c84e0a834c75a645cb7bb847e8e
Cacie Prins <cacie@cypress.io>
2025-01-16 09:40:37 -0500

    fix packages/electron

--- packages/electron/eslint.config.ts
+++ packages/electron/eslint.config.ts
@@ -1,12 +1,11 @@
 import { baseConfig } from '../../eslint.config'
+import globals from 'globals'
 
 export default [
   ...baseConfig, {
     languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['lib/*.js', 'app/*.js', 'index.js', 'test/*.js', 'eslint.config.ts'],
-        },
+      globals: {
+        ...globals.node,
       },
     },
   },
